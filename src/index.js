3ff919471ff6d0836b94c6ca14f1572a8d624aa1
Jesse de Boer <jesse@frsc.nl>
2022-03-31 14:30:35 +0200

    Change to view width

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-var setVh = function () {
-    var svh = document.documentElement.clientHeight * 0.01;
-    document.documentElement.style.setProperty('--1svh', (svh + "px"));
-    var dvh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--1dvh', (dvh + "px"));
+var setVw = function () {
+    var svw = document.documentElement.clientWidth * 0.01;
+    document.documentElement.style.setProperty('--1svw', (svw + "px"));
+    var dvw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty('--1dvw', (dvw + "px"));
 
     if (document.body) {
         var fixed = document.createElement("div");
         fixed.style.width = '1px';
-        fixed.style.height = '100vh';
+        fixed.style.width = '100vw';
         fixed.style.position = 'fixed'
         fixed.style.left = '0';
         fixed.style.top = '0';
@@ -16,13 +16,13 @@ var setVh = function () {
 
         document.body.appendChild(fixed);
 
-        var fixedHeight = fixed.clientHeight;
+        var fixedWidth = fixed.clientWidth;
 
         fixed.remove();
 
-        var lvh = fixedHeight * 0.01;
+        var lvw = fixedWidth * 0.01;
 
-        document.documentElement.style.setProperty('--1lvh', (lvh + "px"));
+        document.documentElement.style.setProperty('--1lvw', (lvw + "px"));
     }
 };
 
@@ -33,16 +33,16 @@ var initialize = function () {
     }
 
     // We run the calculation as soon as possible (eg. the script is in document head)
-    setVh();
+    setVw();
 
     // We run the calculation again when DOM has loaded
     document.addEventListener('DOMContentLoaded', function () {
-        setVh();
+        setVw();
     })
 
     // We run the calculation when window is resized
     window.addEventListener('resize', function () {
-        setVh();
+        setVw();
     });
 }
 
